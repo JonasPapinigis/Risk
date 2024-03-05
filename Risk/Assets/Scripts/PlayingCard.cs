88f382a0ccde634d5ab6d27d742e917003838419
euanmills <euanmills26@gmail.com>
2024-03-05 16:25:32 +0000

    add basic playing card functionality

--- Risk/Assets/Scripts/PlayingCard.cs
+++ Risk/Assets/Scripts/PlayingCard.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 public enum PlayingCardType : int
 {
-    PLAYING_CARD_UNKNOWN = 0
+    INFANTRY = 0,
+    CAVALRY,
+    ARTILLARY
 }
 
-public abstract class PlayingCard
+public class PlayingCard : MonoBehaviour
 {
-    
+    public PlayingCardType design;
+    public TerritoryType territory;
+
+    void Start()
+    {
+        GenerateCard();
+    }
+
+    void Update()
+    {
+
+    }
+
+    // Generate a random design and territory to be allocated to the card.
+    public void GenerateCard()
+    {
+        Random random = new Random();
+        Array terrValues = Enum.GetValues(typeof(TerritoryType));
+        Array designValues = Enum.GetValues(typeof(PlayingCardType));
+        design = (PlayingCardType)designValues.GetValue(
+            random.Next(designValues.Length));
+        territory = (TerritoryType)terrValues.GetValue(
+            random.Next(terrValues.Length));
+    }
 }
