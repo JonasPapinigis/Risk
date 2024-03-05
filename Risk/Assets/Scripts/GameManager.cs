870a549aaa72e2d9c6552f3efc473efa75ba6c05
euanmills <euanmills26@gmail.com>
2024-03-05 15:38:01 +0000

    begin init method

--- Risk/Assets/Scripts/GameManager.cs
+++ Risk/Assets/Scripts/GameManager.cs
@@ -6,9 +6,31 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // list of all players
-    public Player[] players;
+    public List <Player> players = new List<Players>();
     // player currently taking their turn
     public Player currPlayer;
+    // territory manager
+    public Territories territoryManager;
 
-    
+    public Start()
+    {
+        // we need a way in some menu to define how many players
+        // there are
+        Init(3);
+    }
+
+    public Update()
+    {
+
+    }
+
+    public void Init(int nPlayers)
+    {
+        for (int i=0; i < nPlayers; i++) {
+            players.add(new Player();)
+        }
+        territoryManager = new Territories();
+        // we would eventually call this...
+        //territoryManager.AssignTerritories(nPlayers);
+    }
 }
