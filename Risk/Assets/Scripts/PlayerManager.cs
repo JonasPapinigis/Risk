2a60f84278755a17ba388885877dbcaeec87d7fb
JonasPapinigis <111145459+JonasPapinigis@users.noreply.github.com>
2024-04-22 18:40:44 +0100

    Wired GUI

--- Risk/Assets/Scripts/PlayerManager.cs
+++ Risk/Assets/Scripts/PlayerManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TextMeshProUGUI P1Name; public TextMeshProUGUI P1TotalTroops; public TextMeshProUGUI P1TroopsPerTurn; public RectTransform P1Panel;
+    public TextMeshProUGUI P2Name; public TextMeshProUGUI P2TotalTroops; public TextMeshProUGUI P2TroopsPerTurn; public RectTransform P2Panel;
+    public TextMeshProUGUI P3Name; public TextMeshProUGUI P3TotalTroops; public TextMeshProUGUI P3TroopsPerTurn; public RectTransform P3Panel;
+    public TextMeshProUGUI P4Name; public TextMeshProUGUI P4TotalTroops; public TextMeshProUGUI P4TroopsPerTurn; public RectTransform P4Panel;
     void Start()
     {
         
