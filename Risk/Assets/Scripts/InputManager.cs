693093a72fbf77bc6744af77cd2dff0649622442
euanmills <euanmills26@gmail.com>
2024-04-22 15:33:03 +0100

    Update InputManager

--- Risk/Assets/Scripts/InputManager.cs
+++ Risk/Assets/Scripts/InputManager.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InputManager : MonoBehaviour
-{
+{   
+    public Button startButton;
+    public Dropdown playerPicker;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startButton.onClick.AddListener(() => GetDropdownValue());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public int GetDropdownValue()
+    {
+        int selectedIndex = playerPicker.value;
+        string selectedText = playerPicker.options[selectedIndex].text;
+        Debug.Log("Selected Index: " + selectedIndex + ", Selected Text: " + selectedText);
+        return selectedIndex;
     }
 }
