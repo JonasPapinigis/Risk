25b38584e473eb9167d729c3df12e96be52b60c3
JonasPapinigis <111145459+JonasPapinigis@users.noreply.github.com>
2024-04-22 15:09:42 +0100

    Dropdown+Controller

--- Risk/Assets/Scripts/InputManager.cs
+++ Risk/Assets/Scripts/InputManager.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
-{
+{   
+    public Button startButton;
+    public Dropdown playerPicker;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myButton.onClick.AddListener(GetDropdownValue);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public int GetDropdownValue()
+    {
+        int selectedIndex = myDropdown.value;
+        string selectedText = myDropdown.options[selectedIndex].text;
+        Debug.Log("Selected Index: " + selectedIndex + ", Selected Text: " + selectedText);
     }
 }
